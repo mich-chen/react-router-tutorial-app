282f406eb1325ce7164175a9dd7a14309e550d17
Michelle Chen <mich.chen.94@gmail.com>
2023-10-11 17:05:20 -0700

    notes on request.formData()

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -2,11 +2,16 @@ import { Form, useLoaderData, redirect } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 // action is to Update, submitting this form calls UPDATE and From will post action and data auto sync
+// lesson on javascript FormData() object for reqeusts
+// -- React Router prevents this form data body to be sent to server and instead goes to this action function
+// -- each form field has a field called "name" which is accessbile with `formData.get(name)`
+//      firstname = formData.get("first"); etc
+// -- here we are using Object.fromEntries(formData) to collect all {fieldName: value}
 export async function action({ request, params }) {
-  const formData = await request.formData();
+  const formData = await request.formData(); // Map() object
   const updates = Object.fromEntries(formData);
   await updateContact(params.contactId, updates);
-  return redirect(`/contacts/${params.contactId}`);
+  return redirect(`/contacts/${params.contactId}`); // returning "response" that tells the app to change location 
 }
 
 
